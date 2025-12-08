0baef2cabae06ebd24ba094077e173b5f53f5431
AzeemAIDev <azeem.dev.ai@gmail.com>
2025-12-08 21:53:32 +0500

    Improve code structure and readability

--- app.py
+++ app.py
@@ -120,5 +120,5 @@ def predict_news(data : news_input):
 
 # --- 7. Run Uvicorn Server 
 if __name__ == "__main__":
-    # Start FastAPI server locally on port 8000
+    # Start FastAPI server locally on the port 8000
     uvicorn.run(app , host="127.0.0.1" , port=8000)
